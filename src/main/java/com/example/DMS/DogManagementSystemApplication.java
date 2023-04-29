714c0dfc1db02dd9ec1338ddd588ca67f39d057a
Pooja Sura <S555254@nwmissouri.edu>
2023-04-29 18:49:29 -0500

    DMS Project

---
+++ src/main/java/com/example/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 
+ * @author s555254 Pooja Sura
+ *
+ */
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DMS/DogManagementSystemApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.DMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * 
+ * @author s555254 Pooja Sura
+ * 
+ *
+ */
+@SpringBootTest
+class DogManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
